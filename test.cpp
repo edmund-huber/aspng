ea1f25876d239359be4c3bb50d615c747b3c52c7
Edmund Huber <me@ehuber.info>
2019-07-27 22:25:47 -0700

    wip: parse is greedy, single-pass

--- test.cpp
+++ test.cpp
@@ -69,49 +69,44 @@ int main(void) {
     registry.push_back(SinkDevice::create);
     registry.push_back(SourceDevice::create);
 
-    size_t old_assigned_count = 0;
-    while (true) {
-        // Go through all pixels until we find an unassigned pixel, and try to
-        // start a device there.
-        for (size_t y = 0; y < png->get_height(); y++) {
-            for (size_t x = 0; x < png->get_width(); x++) {
-                if (!assigned[x][y * png->get_width()]) {
-                    for (auto di = registry.begin(); di != registry.end(); di++) {
-                        Device *d = (*di)();
-                        // Expand the device in all directions.
-                        expand(png, x, y, d, assigned);
-                    }
+    // Go through all pixels until we find an unassigned pixel, and try to
+    // start a device there.
+    for (size_t y = 0; y < png->get_height(); y++) {
+        for (size_t x = 0; x < png->get_width(); x++) {
+            if (!assigned[x][y * png->get_width()]) {
+                for (auto di = registry.begin(); di != registry.end(); di++) {
+                    Device *d = (*di)();
+                    // Expand the device in all directions.
+                    expand(png, x, y, d, assigned);
                 }
             }
         }
+    }
 
-        // We're done if all the pixels are assigned.
-        size_t assigned_count = 0;
+    // After we have passed through the whole image, every pixel should be
+    // assigned, because everything got parsed out.
+    bool parse_succeeded = true;
+    for (size_t x = 0; x < png->get_width(); x++) {
+        for (size_t y = 0; y < png->get_height(); y++) {
+            if (!assigned[x][y]) {
+                parse_succeeded = false;
+                break;
+            }
+        }
+    }
+
+    // If the parse failed, save the PNG with the recognized pixels masked out,
+    // so the user can see what they did wrong.
+    if (!parse_succeeded) {
         for (size_t x = 0; x < png->get_width(); x++) {
             for (size_t y = 0; y < png->get_height(); y++) {
                 if (assigned[x][y]) {
-                    assigned_count++;
+                    png->set_pixel(x, y, Rgb(0x99, 0x99, 0x99));
                 }
             }
         }
-        if (assigned_count == png->get_width() * png->get_height()) {
-            break;
-        }
-
-        // (If there hasn't been any progress, then we can't parse this image.)
-        if (assigned_count == old_assigned_count) {
-            // Write out the design with the recognized pixels masked out.
-            for (size_t x = 0; x < png->get_width(); x++) {
-                for (size_t y = 0; y < png->get_height(); y++) {
-                    if (assigned[x][y]) {
-                        png->set_pixel(x, y, Rgb(0x99, 0x99, 0x99));
-                    }
-                }
-            }
-            png->write("fail.png");
-            return -1;
-        }
-        old_assigned_count = assigned_count;
+        png->write("fail.png");
+        return -1;
     }
 
     return 0;
