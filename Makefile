f03ba65783983a4656f164f455caf1613c7183e1
Edmund Huber <me@ehuber.info>
2019-12-31 15:02:35 -0800

    WIP: ./bin/aspng builds now, and is a skeleton SDL app

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+.DEFAULT_GOAL=bin/aspng
+
 CXX=g++
 CXXFLAGS=-std=c++17 -pedantic -Wall -Werror -g -O0
+CORE_OBJECTS=obj/background_device.o obj/copper_device.o obj/device.o obj/patch.o obj/png.o obj/pull_device.o obj/sink_device.o obj/source_device.o obj/transistor_device.o
 
 test: bin/test
 	./bin/test
@@ -7,8 +10,11 @@ test: bin/test
 bin:
 	mkdir bin
 
-bin/test: obj/background_device.o obj/copper_device.o obj/device.o obj/patch.o obj/png.o obj/pull_device.o obj/sink_device.o obj/source_device.o obj/test.o obj/transistor_device.o | bin
+bin/test: obj/test.o $(CORE_OBJECTS) | bin
 	$(CXX) $^ -lpng -o $@ -lstdc++fs
+
+bin/aspng: obj/aspng.o $(CORE_OBJECTS) | bin
+	$(CXX) $^ -lpng -o $@ -lstdc++fs -lSDL2
 
 obj:
 	mkdir obj
---
+++ aspng.cpp
@@ -0,0 +1,46 @@
+#include <SDL2/SDL.h>
+
+#include "common.h"
+
+int main(int argc, char **argv) {
+    // Create the SDL window and renderer.
+    ASSERT(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == 0);
+    SDL_Window *sdl_window = SDL_CreateWindow("aspng", 0, 0, 100, 100, 0);
+    ASSERT(sdl_window != nullptr);
+    SDL_Renderer *sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
+    ASSERT(sdl_renderer != nullptr);
+
+    // Event loop!
+    bool do_quit = false;
+    while (!do_quit) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_KEYDOWN:
+                switch(e.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    do_quit = true;
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
+            case SDL_QUIT:
+                do_quit = true;
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(sdl_renderer, 255, 0, 255, 255);
+        SDL_RenderClear(sdl_renderer);
+        SDL_RenderPresent(sdl_renderer);
+    }
+
+    SDL_Quit();
+    return 0;
+}
