2416d422212eb4f0bb384ba20f07daeb5f60abe9
Edmund Huber <me@ehuber.info>
2020-01-01 18:37:40 -0800

    working

--- gui.cpp
+++ gui.cpp
@@ -1,6 +1,73 @@
+#include <iostream>  //////// TODO
 #include <SDL2/SDL.h>
 
+#include "aspng.h"
 #include "common.h"
+#include "png.h"
+
+class SDLAspngSurface : public AspngSurface {
+public:
+    SDLAspngSurface(SDL_Renderer *, size_t, size_t);
+    size_t get_width(void);
+    size_t get_height(void);
+    Rgb get_pixel(size_t, size_t);
+    void set_pixel(size_t, size_t, Rgb);
+    SDL_Texture *get_texture(void);
+
+private:
+    SDL_Texture *sdl_texture;
+};
+
+SDLAspngSurface::SDLAspngSurface(SDL_Renderer *sdl_renderer, size_t w, size_t h) {
+    this->sdl_texture = SDL_CreateTexture(sdl_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, w, h);
+    ASSERT(this->sdl_texture != nullptr);
+}
+
+size_t SDLAspngSurface::get_width(void) {
+    int w, h;
+    SDL_QueryTexture(this->sdl_texture, nullptr, nullptr, &w, &h);
+    return w;
+}
+
+size_t SDLAspngSurface::get_height(void) {
+    int w, h;
+    SDL_QueryTexture(this->sdl_texture, nullptr, nullptr, &w, &h);
+    return h;
+}
+
+SDL_Texture *SDLAspngSurface::get_texture(void) {
+    return this->sdl_texture;
+}
+
+Rgb SDLAspngSurface::get_pixel(size_t x, size_t y) {
+    // SDLAspngSurface is really only meant as an output surface.
+    ASSERT(0);
+}
+
+void SDLAspngSurface::set_pixel(size_t x, size_t y, Rgb rgb) {
+    // Get information about the texture and do bounds-checking.
+    int w, h;
+    uint32_t format;
+    SDL_QueryTexture(this->sdl_texture, &format, nullptr, &w, &h);
+    SDL_PixelFormat *sdl_pixel_format = SDL_AllocFormat(format);
+    ASSERT((x >= 0) && ((int)x < w));
+    ASSERT((y >= 0) && ((int)y < h));
+
+    uint8_t *pixels;
+    int pitch;
+    ASSERT(SDL_LockTexture(this->sdl_texture, nullptr, (void **)&pixels, &pitch) == 0);
+    ASSERT(pixels != nullptr);
+    uint32_t pixel = SDL_MapRGBA(sdl_pixel_format, rgb.r, rgb.g, rgb.b, 255);
+    *((uint32_t *)&pixels[(pitch * y) + (4 * x)]) = pixel;
+    SDL_UnlockTexture(this->sdl_texture);
+    //0xf8, 0xac, 0x59
+    if (rgb.r == 0xf8 && rgb.b != 0xac && rgb.g != 0x59) {
+        std::cout << x << ", " << y << " = " << (int)rgb.r << ", " << (int)rgb.g << ", " << (int)rgb.b << std::endl;
+    }
+    SDL_FreeFormat(sdl_pixel_format);
+}
+
+// TODO: destructor should free stuff
 
 int main(int argc, char **argv) {
     // Create the SDL window and renderer.
@@ -9,6 +76,16 @@ int main(int argc, char **argv) {
     ASSERT(sdl_window != nullptr);
     SDL_Renderer *sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
     ASSERT(sdl_renderer != nullptr);
+
+    auto png = Png::read("tests/nand_gate/_.png");
+    if (png == nullptr)
+        return -1;
+    SDLAspngSurface sdl_aspng_surface(sdl_renderer, png->get_width(), png->get_height());
+
+    std::string error;
+    auto aspng = Aspng(png, error);
+    if (error != "")
+        return -1;
 
     // Event loop!
     bool do_quit = false;
@@ -39,7 +116,22 @@ int main(int argc, char **argv) {
 
         SDL_SetRenderDrawColor(sdl_renderer, 255, 0, 255, 255);
         SDL_RenderClear(sdl_renderer);
+        aspng.draw(&sdl_aspng_surface);
+
+        SDL_Rect sdl_rect_src;
+        sdl_rect_src.w = sdl_aspng_surface.get_width();
+        sdl_rect_src.h = sdl_aspng_surface.get_height();
+        sdl_rect_src.x = 0;
+        sdl_rect_src.y = 0;
+        SDL_Rect sdl_rect_dst;
+        sdl_rect_dst.w = sdl_aspng_surface.get_width() * 8;
+        sdl_rect_dst.h = sdl_aspng_surface.get_height() * 8;
+        sdl_rect_dst.x = 64;
+        sdl_rect_dst.y = 64;
+        SDL_RenderCopy(sdl_renderer, sdl_aspng_surface.get_texture(), &sdl_rect_src, &sdl_rect_dst);
         SDL_RenderPresent(sdl_renderer);
+
+        SDL_Delay(100);
     }
 
     SDL_Quit();
